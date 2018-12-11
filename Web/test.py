552050987689708eafe83454c76edbb23a195516
aaronyu <ayu@workfusion.com>
2018-12-10 20:52:29 -0500

    decorator

--- Web/test.py
+++ Web/test.py
@@ -1,3 +1,12 @@
-if __name__ == '__main__':
-    print("Hello World")
-    print("ok")
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return "Hello, World!"
+
+
+if __name__ == "__main__":
+    app.run()
