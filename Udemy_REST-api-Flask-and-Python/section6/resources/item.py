52eac90a27df6cdf71c282ee0587984c4bca3d54
aaronyu <ayu@workfusion.com>
2018-12-14 15:30:51 -0500

    Udemy Flask - section 6 final

--- Udemy_REST-api-Flask-and-Python/section6/resources/item.py
+++ Udemy_REST-api-Flask-and-Python/section6/resources/item.py
@@ -1,9 +1,7 @@
-from flask_jwt import jwt_required
 from flask_restful import Resource, reqparse
+from flask_jwt import jwt_required
 from models.item import ItemModel
 
-
-# import sqlite3
 
 class Item(Resource):
     parser = reqparse.RequestParser()
@@ -15,94 +13,53 @@ class Item(Resource):
     parser.add_argument('store_id',
                         type=int,
                         required=True,
-                        help="Every item needs a store id"
+                        help="Every item needs a store_id."
                         )
 
     @jwt_required()
     def get(self, name):
-        try:
-            item = ItemModel.find_by_name(name)
-        except:
-            return {"message": "An error occurred when getting an item"}
+        item = ItemModel.find_by_name(name)
         if item:
             return item.json()
-        return {"message": "Item not found"}, 404
+        return {'message': 'Item not found'}, 404
 
     def post(self, name):
         if ItemModel.find_by_name(name):
-            return {'message': "An item with name '{}' already exists.".format(name)}
+            return {'message': "An item with name '{}' already exists.".format(name)}, 400
 
         data = Item.parser.parse_args()
-        item = ItemModel(name, data['price'], data['store_id'])
+
+        item = ItemModel(name, **data)
+
         try:
             item.save_to_db()
         except:
-            return {"message": "An error occurred inserting the item"}, 500
+            return {"message": "An error occurred inserting the item."}, 500
 
         return item.json(), 201
 
-    ## DELETE
-    # @jwt_required()
     def delete(self, name):
         item = ItemModel.find_by_name(name)
         if item:
             item.delete_from_db()
-        return {'message': "Item deleted"}
+            return {'message': 'Item deleted.'}
+        return {'message': 'Item not found.'}, 404
 
-        # connection = sqlite3.connect("data.db")
-        # cursor = connection.cursor()
-        #
-        # query = "DELETE FROM items WHERE name =?"
-        # cursor.execute(query, (name,))
-        #
-        # connection.commit()
-        # connection.close()
-        #
-        # return {'message': "Item deleted"}
-
-    ## PUT
-    # @jwt_required()
     def put(self, name):
         data = Item.parser.parse_args()
+
         item = ItemModel.find_by_name(name)
 
-        if item is None:
-            item = ItemModel(name, data['price'], data['store_id'])
-        else:
+        if item:
             item.price = data['price']
+        else:
+            item = ItemModel(name, **data)
 
         item.save_to_db()
-        return item.json()
 
-        # # Once again, print something not in the args to verify everything works
-        # item = ItemModel.find_by_name(name)
-        # updated_item = ItemModel(name, data['price'])
-        # if item is None:
-        #     try:
-        #         updated_item.insert()
-        #     except:
-        #         return {"message": "An error occurred inserting the item."}
-        # else:
-        #     try:
-        #         updated_item.update()
-        #     except:
-        #         return {"message": "An error occurred updating the item."}
-        # return updated_item.json()
+        return item.json()
 
 
 class ItemList(Resource):
     def get(self):
-        return {"items": [item.json() for item in ItemModel.query.all()]}
-        # connection = sqlite3.connect("data.db")
-        # cursor = connection.cursor()
-        #
-        # query = "SELECT * FROM items"
-        # result = cursor.execute(query)
-        # items = []
-        # for row in result:
-        #     items.append({"name": row[0], "price":row[1]})
-        #
-        # connection.commit()
-        # connection.close()
-
-        # return {"items": items}
+        return {'items': list(map(lambda x: x.json(), ItemModel.query.all()))}
--- Udemy_REST-api-Flask-and-Python/section6/resources/store.py
+++ Udemy_REST-api-Flask-and-Python/section6/resources/store.py
@@ -1,51 +1,34 @@
-from flask_restful import Resource, reqparse
+from flask_restful import Resource
 from models.store import StoreModel
 
 
 class Store(Resource):
-    # parser = reqparse.RequestParser()
-    # parser.add_argument('price',
-    #                     type=float,
-    #                     required=True,
-    #                     help="This field cannot be left blank!"
-    #                     )
-    # parser.add_argument('store_id',
-    #                     type=int,
-    #                     required=True,
-    #                     help="Every item needs a store id"
-    #                     )
-
-    # @jwt_required()
     def get(self, name):
-        try:
-            store = StoreModel.find_by_name(name)
-        except:
-            return {"message": "An error occurred when getting an item"}
+        store = StoreModel.find_by_name(name)
         if store:
             return store.json()
-        return {"message": "Store not found"}, 404
+        return {'message': 'Store not found'}, 404
 
     def post(self, name):
         if StoreModel.find_by_name(name):
-            return {'message': "An store with name '{}' already exists.".format(name)}, 400
+            return {'message': "A store with name '{}' already exists.".format(name)}, 400
 
         store = StoreModel(name)
         try:
             store.save_to_db()
         except:
-            return {"message": "An error occurred creating the store"}, 500
+            return {"message": "An error occurred creating the store."}, 500
 
         return store.json(), 201
 
-    ## DELETE
-    # @jwt_required()
     def delete(self, name):
         store = StoreModel.find_by_name(name)
         if store:
             store.delete_from_db()
-        return {'message': "Item deleted"}
+
+        return {'message': 'Store deleted'}
 
 
 class StoreList(Resource):
     def get(self):
-        return {"stores": [store.json() for store in StoreModel.query.all()]}
+        return {'stores': list(map(lambda x: x.json(), StoreModel.query.all()))}
