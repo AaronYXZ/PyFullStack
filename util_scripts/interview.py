d68292a2e34a0add4e0ef43fdeb9c2b86fdfe141
aaronyu <ayu@workfusion.com>
2019-04-24 10:45:43 -0400

    interview

--- util_scripts/interview.py
+++ util_scripts/interview.py
@@ -3,42 +3,97 @@
 
 import unittest
 
-# Q1 - You have a list of lists. Convert it to a flat list. For example, [[1,2], [3,4]] to [1,2,3,4]
+import unittest
 
-list_2D = [[1, 2, 3], ["a", 4, 5], ["b", 6]]
+# Q1 - You have a list of lists. Convert it to a flat list. Only keep the element that's either a string, or an integer that can be divided by 2.
+# For example, [[1,2], [3,4, "a"]] to [2,4,"a"]
+
+list_2D = [[1, 2], [3, 4, "a"]]
 
 
 def Q1(list_2D):
     pass
 
 
-# Q2 - You have a sequence of items, and you’d like to determine the most frequently occurring items in the sequence.
+# Q2 - You have a sequence of items, and you’d like to determine the most frequently occurring items in the sequence. Return them as a tuple (item, # of occurrence). There will only be one unique pair
+# For example, ['look', 'into', 'my', 'eyes', 'look'] -> ("look", 2)
 
 words = ['look', 'into', 'my', 'eyes', 'look', 'into', 'my', 'eyes',
          'the', 'eyes', 'the', 'eyes', 'the', 'eyes', 'not', 'around', 'the',
          'eyes', "don't", 'look', 'around', 'the', 'eyes', 'look', 'into',
-         'my', 'eyes', "you're", 'under']
+         'my', 'eyes', 'you', 'are', 'under']
 
-def Q2():
+
+def Q2(words):
     pass
 
 
-# Q4 - Design a Class
-#   __init__(self, data):
-#      _check_
+# Q3 - Use DataFrame from pandas
+data = {"name": ["Eric", "Emma", "Ted", "Ashley"],
+        "age": [19, 22, 25, 18],
+        "score": [95.0, 88.5, 72.0, 91.3]}
+# create a pandas DataFrame out of the dictionary
+
+# get the columns as a list
+
+# change the last column names to "final_score"
+
+# sort the df by age, in ascending order
+
+# Q4 - Design a Class DataFrameDemo
+
+"""
+ input data is a dictionary, all its keys should be strings, values should be ndarray of the same length
+ names = np.array(["Eric", "Emma", "Ted", "Ashley"])
+ ages = np.array([19, 22, 25, 18])
+ scores = np.array([95.0, 88.5, 72.0, 91.3])
+ data = {"name": names , "age": ages, "score": scores}
+"""
 
 
-list_1D = Q1(list_2D)
+class DataFrameDemo:
+
+    def __init__(self, data):
+        self._check_input_type(data)
+        self._check_input_length(data)
+        self._data = data
+
+    # check that input data is dictionary, it's keys are strings, values are ndarray
+    def _check_input_type(self, data):
+        pass
+
+    # check that all ndarray in the input data have the same length
+    def _check_input_length(self, data):
+        pass
+
+    # return column names as a list, in any order
+    def get_columns(self):
+        pass
+
+    # return a DataFrame sorted by the column specified
+    def sort_by(self, by_column, ascending=True):
+        pass
+
+
+################################################################################################
+
+result1 = Q1(list_2D)
+result2 = Q2(words)
 
 
 class Test(unittest.TestCase):
-    def test_len(self):
-        self.assertEqual(len(list_1D), 5)
+    def test1_len(self):
+        self.assertEqual(len(result1), 3)
 
-    def test_component(self):
-        self.assertTrue("a" in list_1D)
-        self.assertTrue("b" in list_1D)
-        self.assertTrue(2 in list_1D)
-        self.assertFalse(1 in list_1D)
+    def test1_component(self):
+        self.assertTrue("a" in result1)
+        self.assertTrue(2 in result1)
+        self.assertFalse(5 in result1)
 
-# unittest.main()
+    def test2_istuple(self):
+        self.assertTrue(isinstance(result2, tuple))
+        self.assertEqual(result2[0], "eyes")
+        self.assertEqual(result2[1], 8)
+
+
+unittest.main()
--- util_scripts/interview_answer.py
+++ util_scripts/interview_answer.py
@@ -1,13 +1,14 @@
 # Libraries Included:
 # Numpy, Scipy, Scikit, Pandas
 
+import pandas as pd
 from collections import defaultdict, Counter
 import unittest
 
 # Q1 - You have a list of lists. Convert it to a flat list. Only keep the element that's either a string, or an integer that can be divided by 2.
 # For example, [[1,2], [3,4, "a"]] to [2,4,"a"]
 
-list_2D = [[1, 2, 3], ["a", 4, 5], ["b", 6]]
+list_2D = [[1, 2], [3, 4, "a"]]
 
 
 def Q1(list_2D):
@@ -43,31 +44,60 @@ def Q2(words):
     return word, count
 
 
-# Q3 -
+# Q3 - Use DataFrame from pandas
+data = {"name": ["Eric", "Emma", "Ted", "Ashley"],
+        "age": [19, 22, 25, 18],
+        "score": [95.0, 88.5, 72.0, 91.3]}
+# create a pandas DataFrame out of the dictionary
+df = pd.DataFrame(data)
+
+# get the columns as a list
+df.columns.tolist()
+
+# change the last column names to "final_score"
+df.columns = ["name", "age", "final_score"]
+
+# sort the df by age, in ascending order
+df.sort_values(by = "age")
+
 # Q4 - Design a Class DataFrameDemo
+
+"""
+ input data is a dictionary, all its keys should be strings, values should be ndarray of the same length
+ names = np.array(["Eric", "Emma", "Ted", "Ashley"])
+ ages = np.array([19, 22, 25, 18])
+ scores = np.array([95.0, 88.5, 72.0, 91.3])
+ data = {"name": names , "age": ages, "score": scores}
+"""
+
 class DataFrameDemo:
-    # data should be a dictionary, all its keys should be strings, values should be lists of the same length
-    # for exmaple, data = {"name": ["Jeff", "Emma", "Mike", "Jo"], "age": [21, 19, 22, 20], "enrolled": [false, true, true, false]}
+
     def __init__(self, data):
         self._check_input_type(data)
         self._check_input_length(data)
         self._data = data
 
+    # check that input data is dictionary, it's keys are strings, values are ndarray
     def _check_input_type(self, data):
         if not isinstance(data, dict):
             raise TypeError("data must be a dictionary")
         for col_name, values in data.items():
             if not isinstance(col_name, str):
-                raise TypeError("all columns must be strings")
+                raise TypeError("all column names must be strings")
             if not isinstance(values, list):
-                raise TypeError()
+                raise TypeError("all column values must be list")
 
-
+    # check that all ndarray in the input data have the same length
     def _check_input_length(self, data):
         pass
 
+    # return column names as a list, in any order
+    def get_columns(self):
+        pass
 
-    def columns(self):
+    # return a DataFrame sorted by the column specified
+
+    def sort_by(self, by_column, ascending = True):
         pass
 
 
@@ -79,17 +109,14 @@ result1 = Q1(list_2D)
 result2 = Q2(words)
 
 
-
-
-class Test_Q1(unittest.TestCase):
+class Test(unittest.TestCase):
     def test1_len(self):
-        self.assertEqual(len(result1), 5)
+        self.assertEqual(len(result1), 3)
 
     def test1_component(self):
         self.assertTrue("a" in result1)
-        self.assertTrue("b" in result1)
         self.assertTrue(2 in result1)
-        self.assertFalse(1 in result1)
+        self.assertFalse(5 in result1)
 
     def test2_istuple(self):
         self.assertTrue(isinstance(result2, tuple))
