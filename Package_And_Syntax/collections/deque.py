6adc2466f99fc66e5e93a0d5fc44d0b4be28f98f
aaronyu <ayu@workfusion.com>
2019-06-05 11:25:05 -0400

    collections

--- Package_And_Syntax/collections/deque.py
+++ Package_And_Syntax/collections/deque.py
@@ -16,3 +16,12 @@ queue.popleft()
 queue.popleft()
 print(queue)
 
+deque_demo = deque(maxlen= 3)
+deque_demo.append(1)
+deque_demo.append(2)
+deque_demo.append(3)
+deque_demo.append(4)
+print(deque_demo)
+deque_demo.appendleft(0)
+print(deque_demo)
+
---
+++ Projects/Web_udemy_rest_api_with_flask_and_python/section3/app.py
@@ -0,0 +1,5 @@
+from flask import Flask
+# package starts with lower case, class starts with upper case
+
+app = Flask(__name__)
+
