2d7133804cd499c56bdaf72b44aaad5dbbad51dd
aaronyu <ayu@workfusion.com>
2019-07-09 21:59:55 -0400

    check in

--- Package_And_Syntax/generic_syntax/iterator_vs_generator.py
+++ Package_And_Syntax/generic_syntax/iterator_vs_generator.py
@@ -10,11 +10,13 @@ def generator_count_down(num):
 class test_iterator_generator(unittest.TestCase):
 
     def test_iterator_generator(self):
+        ## generator is subclass of iterator, iterator is subclass of iterable
         self.assertTrue(issubclass(Generator, Iterator))
         self.assertFalse(issubclass(Iterator, Generator))
         self.assertTrue(issubclass(Generator, Iterable))
 
     def test_generator(self):
+        ## both are ways to construct generators
         g = generator_count_down(5)
         self.assertTrue(isinstance(g, Generator))
         g2 = (x for x in range(5) if x % 2 == 0)
