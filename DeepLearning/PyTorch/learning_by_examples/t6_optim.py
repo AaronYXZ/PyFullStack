68db69e6e63a7cb5b9c50984331dfb3e68ef1b8e
aaronyu <ayu@workfusion.com>
2019-07-25 13:59:58 -0400

    pytorch tutorial - learning by examples

---
+++ DeepLearning/PyTorch/learning_by_examples/t6_optim.py
@@ -0,0 +1,30 @@
+import torch
+
+# N is batch size; D_in is input dimension;
+# H is hidden dimension; D_out is output dimension.
+N, D_in, H, D_out = 64, 1000, 100, 10
+
+# Create random Tensors to hold inputs and outputs
+x = torch.randn(N, D_in)
+y = torch.randn(N, D_out)
+
+# Use the nn package to define our model and loss function.
+model = torch.nn.Sequential(
+    torch.nn.Linear(D_in, H),
+    torch.nn.ReLU(),
+    torch.nn.Linear(H, D_out),
+)
+loss_fn = torch.nn.MSELoss(reduction='sum')
+
+learning_rate = 1e-4
+
+optimizer = torch.optim.Adam(model.parameters(), lr = learning_rate)
+
+for t in range(500):
+    y_pred = model(x)
+    loss = loss_fn(y, y_pred)
+
+    optimizer.zero_grad()
+
+    loss.backward()
+    optimizer.step()
