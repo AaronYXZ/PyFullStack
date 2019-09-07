ea5db0e0807f275b9d1ebf105b9cfe53d5ef4890
aaronyu <ayu@workfusion.com>
2019-09-07 18:34:42 -0400

    Leetcode init

---
+++ util_scripts/python_cookbook/C4_Iterator_Generator/s2_implement_iter.py
@@ -0,0 +1,13 @@
+class Node:
+    def __init__(self, value):
+        self._value = value
+        self._children = []
+
+    def __repr__(self):
+        return 'Node({!r}'.format(self._value)
+
+    def add_child(self, node):
+        self._children.append(node)
+
+    def __iter__(self):
+        pass
